923f27c8acce46ec03fb012298c35657a5844620
vict0rsch <vsch@protonmail.com>
2019-06-05 15:08:50 +0200

    scrolling depending on visibility of cat + adaptative scroll time

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -2,12 +2,23 @@ jQuery(function () {
     const $ = jQuery;
     $(".gallery-nav li a").click((e) => {
         const cat = $(e.target).data("cat");
-        if ([2017, 2018].indexOf(cat) > -1) {
-            const el = $(`.gallery .${cat}`).first();
-            const scrollTime = cat === 2017 ? 800 : 500;
+        if (cat === "tous") return;
+
+        const el = $(`.gallery .${cat}`).first();
+        const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        const p = el.position();
+
+        if (p.top > h) {
+
+            let scrollTime = Math.round((p.top - h) / 3);
+            scrollTime = Math.min(scrollTime, 800);
+            scrollTime = Math.max(scrollTime, 300);
+            // 300 < scrollTime < 800 (ms)
+
             $([document.documentElement, document.body]).animate({
                 scrollTop: el.offset().top - 100
             }, scrollTime);
         }
+
     });
 });
