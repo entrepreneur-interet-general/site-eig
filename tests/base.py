22014acb736fbbc830a8a94f236c46959e65c0aa
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-08-06 09:13:03 +0200

    Add tests for posts #248

---
+++ tests/base.py
@@ -0,0 +1,18 @@
+import unittest
+
+import yaml
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        with open("_data/defis.yml") as f:
+            self.defis = yaml.safe_load(f)
+
+        with open("_data/personnes.yml") as f:
+            self.personnes = yaml.safe_load(f)
+
+    def clean_md_filename(self, filename, prefix):
+        file_no_prefix = filename.replace(f"_{prefix}/", "")
+        year = file_no_prefix.split("-")[0]
+        slug = "-".join(file_no_prefix.split("-")[3:]).replace(".md", "")
+        return f"/{prefix}/{year}/{slug}.html"
